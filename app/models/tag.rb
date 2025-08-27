b7973a00d68d7ba3025fd9b2394bb3a959b0a0c5
Eric Laquer <LaquerEric@gmail.com>
2025-08-27 19:35:22 -0400

    Admin>Tags implemented

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -13,4 +13,13 @@ class Tag < ApplicationRecord
   def usage_count
     documents.count
   end
+
+  # Ransack configuration
+  def self.ransackable_attributes(auth_object = nil)
+    ["color", "created_at", "id", "id_value", "name", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["document_tags", "documents"]
+  end
 end
