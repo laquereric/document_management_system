cb60691db32ebac6a14c871ec48c3dff2e39e33c
Eric Laquer <LaquerEric@gmail.com>
2025-08-27 18:37:27 -0400

    Sidebar Admin - Users implmented

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,28 @@
 class Admin::DashboardController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_admin!
+
   def index
+    @total_users = User.count
+    @total_organizations = Organization.count
+    @total_teams = Team.count
+    @total_documents = Document.count
+    @total_folders = Folder.count
+    @total_tags = Tag.count
+    
+    @recent_users = User.order(created_at: :desc).limit(5)
+    @recent_documents = Document.includes(:author, :team).order(created_at: :desc).limit(5)
+    @recent_activity = ActivityLog.includes(:user, :document).order(created_at: :desc).limit(10)
+    
+    @users_by_role = User.group(:role).count
+    @documents_by_status = Document.joins(:status).group('statuses.name').count
+  end
+
+  private
+
+  def require_admin!
+    unless current_user&.admin?
+      redirect_to root_path, alert: 'Access denied. Admin privileges required.'
+    end
   end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,16 +1,76 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_admin!
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :toggle_role]
+
   def index
+    @q = User.ransack(params[:q])
+    @users = @q.result(distinct: true)
+               .includes(:organization, :teams)
+               .order(created_at: :desc)
+               .page(params[:page])
+               .per(20)
   end
 
   def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      redirect_to admin_user_path(@user), notice: 'User was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: 'User was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @user == current_user
+      redirect_to admin_users_path, alert: 'You cannot delete your own account.'
+    else
+      @user.destroy
+      redirect_to admin_users_path, notice: 'User was successfully deleted.'
+    end
   end
 
   def toggle_role
+    new_role = @user.role == 'admin' ? 'member' : 'admin'
+    if @user.update(role: new_role)
+      redirect_to admin_user_path(@user), notice: "User role changed to #{new_role}."
+    else
+      redirect_to admin_user_path(@user), alert: 'Failed to change user role.'
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :role, :organization_id, :password, :password_confirmation)
+  end
+
+  def require_admin!
+    unless current_user&.admin?
+      redirect_to root_path, alert: 'Access denied. Admin privileges required.'
+    end
   end
 end
