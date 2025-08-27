02c1fa33cec0e560b491e6221bab8c9e591dc9e0
Eric Laquer <LaquerEric@gmail.com>
2025-08-27 19:45:47 -0400

    Admin>Scenarios implemented

--- app/models/scenario_type.rb
+++ app/models/scenario_type.rb
@@ -9,4 +9,13 @@ class ScenarioType < ApplicationRecord
   def documents_count
     documents.count
   end
+
+  # Ransack configuration
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "description", "id", "id_value", "name", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["documents"]
+  end
 end
