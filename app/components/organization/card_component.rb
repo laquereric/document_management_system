ac29d80e20bbe1e3f92e63e7a69ade4c35242a58
Eric Laquer <LaquerEric@gmail.com>
2025-08-29 09:28:29 -0400

    admin team and organization

--- app/components/organization/card_component.rb
+++ app/components/organization/card_component.rb
@@ -1,29 +1,18 @@
 class Organization::CardComponent < ApplicationComponent
   include CardConcerns
 
-  def initialize(organization:, show_actions: true, **system_arguments)
+  def initialize(organization:, show_actions: true, admin_context: false, **system_arguments)
     @organization = organization
+    @admin_context = admin_context
     initialize_card_base(show_actions: show_actions, **system_arguments)
   end
 
   private
 
-  attr_reader :organization
+  attr_reader :organization, :admin_context
 
   def card_classes
     "#{condensed_card_classes} h-full #{system_arguments[:class]}".strip
-  end
-
-  def title_classes
-    "#{header_classes} border-bottom-0 pb-2"
-  end
-
-  def body_classes
-    "#{super} pt-0 pb-3"
-  end
-
-  def footer_classes
-    "#{super} border-top pt-3"
   end
 
   def truncated_description
@@ -36,5 +25,9 @@ class Organization::CardComponent < ApplicationComponent
 
   def organization_name
     safe_name(organization, "Unknown organization")
+  end
+
+  def admin_context?
+    admin_context
   end
 end
--- app/components/teams/team_card_component.rb
+++ app/components/teams/team_card_component.rb
@@ -1,15 +1,16 @@
 class Teams::TeamCardComponent < ApplicationComponent
   include CardConcerns
 
-  def initialize(team:, current_user: nil, **system_arguments)
+  def initialize(team:, current_user: nil, admin_context: false, **system_arguments)
     @team = team
     @current_user = current_user
+    @admin_context = admin_context
     initialize_card_base(show_actions: true, **system_arguments)
   end
 
   private
 
-  attr_reader :team, :current_user
+  attr_reader :team, :current_user, :admin_context
 
   def card_classes
     "#{base_card_classes} h-100 #{system_arguments[:class]}".strip
@@ -38,5 +39,9 @@ class Teams::TeamCardComponent < ApplicationComponent
 
   def organization_name
     safe_name(team.organization, "No Organization")
+  end
+
+  def admin_context?
+    admin_context
   end
 end
