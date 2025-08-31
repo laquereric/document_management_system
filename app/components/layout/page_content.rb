468bb503cd6192fc3a5d28b33f3e9f6eb1892950
Eric Laquer <LaquerEric@gmail.com>
2025-08-31 11:10:43 -0400

    page_content refactor

---
+++ app/components/layout/page_content.rb
@@ -0,0 +1,35 @@
+class Layout::PageContent < ApplicationComponent
+
+  def initialize(
+    padding: :default,
+    **system_arguments
+  )
+    @padding = padding
+    super(**system_arguments)
+  end
+
+  private
+
+  attr_reader :padding
+
+  def container_classes
+    base_classes = "container-lg px-3"
+    
+    case padding
+    when :none
+      base_classes
+    when :small
+      "#{base_classes} py-2"
+    when :default
+      "#{base_classes} py-4"
+    when :large
+      "#{base_classes} py-6"
+    else
+      base_classes
+    end
+  end
+
+  def render?
+    content.present?
+  end
+end
