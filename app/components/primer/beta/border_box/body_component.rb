e445cd2c32741285bd47d2c594eb01034f322ae2
Eric Laquer <LaquerEric@gmail.com>
2025-09-03 01:06:55 -0400

    fixed paths4

---
+++ app/components/primer/beta/border_box/body_component.rb
@@ -0,0 +1,13 @@
+class Primer::Beta::BorderBox::Body < ApplicationComponent
+  def initialize(**system_arguments)
+    @system_arguments = merge_system_arguments(system_arguments)
+  end
+
+  private
+
+  attr_reader :system_arguments
+
+  def body_classes
+    "Box-body #{system_arguments[:class]}"
+  end
+end
---
+++ app/components/primer/beta/border_box/row_component.rb
@@ -0,0 +1,13 @@
+class Primer::Beta::BorderBox::Row < ApplicationComponent
+  def initialize(**system_arguments)
+    @system_arguments = merge_system_arguments(system_arguments)
+  end
+
+  private
+
+  attr_reader :system_arguments
+
+  def row_classes
+    "Box-row #{system_arguments[:class]}"
+  end
+end
