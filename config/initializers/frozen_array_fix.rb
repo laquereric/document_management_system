6e304f0a1e667cd3fbc700446d55cf7df3c7755c
Eric Laquer <LaquerEric@gmail.com>
2025-09-04 12:39:22 -0400

    updated

--- config/initializers/frozen_array_fix.rb
+++ config/initializers/frozen_array_fix.rb
@@ -1,20 +1,30 @@
 # Fix for Rails 8 frozen array issues in test environment
 # This initializer handles frozen arrays that cause issues with Zeitwerk and Bootsnap
 
-#TODO - This messes up the test environment. Constants will not load!
-
-=begin
 if Rails.env.test?
   # Fix Zeitwerk frozen array issues
   if defined?(Zeitwerk::Loader)
     Zeitwerk::Loader.class_eval do
-      def push_dir(dir)
-        if @dirs.nil?
-          @dirs = []
-        elsif @dirs.frozen?
-          @dirs = @dirs.dup
+      def push_dir(path, namespace: Object)
+        unless namespace.is_a?(Module) # Note that Class < Module.
+          raise Zeitwerk::Error, "#{namespace.inspect} is not a class or module object, should be"
         end
-        @dirs << dir
+
+        unless real_mod_name(namespace)
+          raise Zeitwerk::Error, "root namespaces cannot be anonymous"
+        end
+
+        abspath = File.expand_path(path)
+        if dir?(abspath)
+          raise_if_conflicting_directory(abspath)
+          if @roots.frozen?
+            puts "Zeitwerk::Loader.push_dir: @roots is frozen, duplicating"
+            @roots = @roots.dup 
+          end
+          @roots[abspath] = namespace
+        else
+          raise Zeitwerk::Error, "the root directory #{abspath} does not exist"
+        end
       end
     end
   end
@@ -38,4 +48,3 @@ if Rails.env.test?
     end
   end
 end
-=end
