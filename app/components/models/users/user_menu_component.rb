aa4a90d24f8d217412bfb2e7e7217caaaed34bd0
Eric Laquer <LaquerEric@gmail.com>
2025-09-04 12:56:21 -0400

    server runs

--- app/components/models/users/user_menu_component.rb
+++ app/components/models/users/user_menu_component.rb
@@ -58,10 +58,7 @@ class Models::Users::UserMenuComponent < ApplicationComponent
     {
       menu_items: menu_items,
       user_initials: user_initials,
-      user: user,
-      destroy_user_session_path: helpers.destroy_user_session_path,
-      user_path: helpers.user_path,
-      edit_user_path: helpers.edit_user_path
+      user: user
     }
   end
 end
